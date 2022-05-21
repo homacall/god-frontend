42d92cee91f950feb68a47e20a55368b87122fe7
zahratgh <zahrataghavi98@gmail.com>
2022-05-21 12:02:05 +0430

    FRONTEND-31-add-title add title

--- src/ui.homacall.god/container/publicRoutes.jsx
+++ src/ui.homacall.god/container/publicRoutes.jsx
@@ -1,4 +1,6 @@
 import { Route, Routes } from 'react-router'
+import { Helmet } from 'react-helmet'
+
 import { authRoutes } from '../utils/constants/routes/publicRoute'
 import LoginForm from '../module/auth/login/loginForm'
 
@@ -6,7 +8,18 @@ export const PublicRoutes = () => {
   return (
     <div className={`w-full bg-slate-100 inline-block `}>
       <Routes>
-        <Route path={`${authRoutes.login}`} element={<LoginForm />} />
+        <Route
+          path={`${authRoutes.login}`}
+          element={
+            <>
+              <Helmet>
+                <meta charSet="utf-8" />
+                <title>ورود</title>
+              </Helmet>
+              <LoginForm />
+            </>
+          }
+        />
       </Routes>
     </div>
   )
