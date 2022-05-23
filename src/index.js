bad79df1fca413915ff4cecc83307934c77be7d6
MeiSaM25970 <67014411+MeiSaM25970@users.noreply.github.com>
2022-05-23 09:26:33 +0430

    debug request

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RecoilRoot } from 'recoil';
-import {BrowserRouter} from 'react-router-dom'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { RecoilRoot } from 'recoil'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
 
-  </React.StrictMode>
-);
-
-reportWebVitals();
+reportWebVitals()
