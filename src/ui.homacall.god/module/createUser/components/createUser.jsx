93a023908311027145510ba3fb4874a4b7c2391e
zahratgh <85282383+zahratgh@users.noreply.github.com>
2022-08-03 12:42:32 +0430

    change create user result

--- src/ui.homacall.god/module/createUser/components/createUser.jsx
+++ src/ui.homacall.god/module/createUser/components/createUser.jsx
@@ -219,7 +219,7 @@ const CreateAndEditUser = () => {
               ToastAlert.error('خطا در ثبت تصویر کاربر')
             } else if (res.data.message === 'JustUploadUser') {
               ToastAlert.error('خطا در ثبت مشخصات کاربر')
-            } else if (res.data.message === 'Succeed') {
+            } else if (res.data.message === 'Sucess') {
               ToastAlert.success('کاربر با موفقیت ثبت شد')
             } else {
               ToastAlert.error('خطا در ثبت کاربر جدید')
